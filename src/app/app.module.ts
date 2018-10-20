62f1f81104b308b81e3f3f3719f8b6abe63dd5e1
DESKTOP-7JO3UEO\huang <huanghunz@hotmail.com>
2018-10-20 01:56:11 -0700

    register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule} from '@angular/router'
+import {ROUTES} from './routes/routes'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+
+import {FormsModule} from "@angular/forms"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,9 @@
+import {RegisterComponent} from './../register/register.component'
+
+
+export const ROUTES = [
+    {
+        path:'auth/register',
+        component: RegisterComponent
+    }
+]
