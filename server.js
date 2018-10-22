29ba257cdbc1863efa5b35eac5f4f902f1393bc6
DESKTOP-7JO3UEO\huang <huanghunz@hotmail.com>
2018-10-21 20:40:04 -0700

    add get request

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + "/dist"))
+app.use(express.static(__dirname + "/dist/jokeApp"))
 
-
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'dist/jokeApp/index.html'));
+})
 
 app.listen(process.env.PORT || 8080)
