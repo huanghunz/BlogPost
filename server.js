b37696e73517860c43a1a36f3f41d4af76658761
DESKTOP-7JO3UEO\huang <huanghunz@hotmail.com>
2018-10-21 20:12:39 -0700

    add dependencies

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
-//const path = require('path')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + "/dist"))
+app.use(express.static(__dirname + "/dist/jokeApp"))
 
 app.listen(process.env.PORT || 8080)
