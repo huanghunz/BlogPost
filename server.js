1156df741ce4ff9eed6ff82be27fb4267f69ad16
DESKTOP-7JO3UEO\huang <huanghunz@hotmail.com>
2018-10-21 20:27:57 -0700

    change postinstall command

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + "/dist/jokeApp"))
+app.use(express.static(__dirname + "/dist"))
+
+
 
 app.listen(process.env.PORT || 8080)
